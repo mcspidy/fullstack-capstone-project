4effe2df7190ba16b5029146226ea71692df5594
Ron Phillips <mcspidy@gmail.com>
2024-12-27 11:48:31 -0600

    Updated .gitignore to exclude misc folder & files

--- giftlink-backend/util/import-mongo/misc.py
+++
@@ -1,3 +0,0 @@
-"""
-[10:26:38.542] [server] Checking /home/mcspidy/.vscode-server/cli/servers/Stable-fabdb6a30b49f79a7aba0f2ad9df9b399473380f/log.txt and /home/mcspidy/.vscode-server/cli/servers/
-"""
